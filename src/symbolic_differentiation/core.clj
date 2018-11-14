de53ac08d3333e77457106e703a179c02c8b0d6c
jakubiakm <maciej190509@gmail.com>
2018-11-14 21:41:56 +0100

    pochodna stałej

--- src/symbolic_differentiation/core.clj
+++ src/symbolic_differentiation/core.clj
@@ -4,8 +4,16 @@
   (:require [utils.math :as cm]) ; cm as Custom Math
 ) 
 
+(defn differentiation 
+  [expression variable]
+  (cond 
+    (number? expression) 0 ; d/dx const = 0
+  )
+)
+
 (defn -main
   [& args]
+  (println (differentiation 4 'x'))
   (def x 3)
   (println (+ 2 3))
   (println "Hello, World!")
--- test/symbolic_differentiation/core_test.clj
+++ test/symbolic_differentiation/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [symbolic-differentiation.core :refer :all]))
 
-(deftest a-test
+(deftest constant-diff-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 (differentiation 1 "x")))
+    (is (= 0 (differentiation -1000 "x")))
+    (is (= 0 (differentiation (* 3 -3) "y")))
+    (is (= 0 (differentiation 0 "abc")))    
+  )
+)
+
